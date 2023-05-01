c221af149940914521c253300803ef6346fdbf62
James Chung <james@chung.contact>
2023-05-01 01:49:04 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ if [ ! -x "$(command -v brew)" ]; then
             eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
             ;;
         Darwin*)
-            
+            # TODO: Fill this in later
             ;;
     esac
 fi
@@ -20,7 +20,7 @@ if [ -x "$(command -v nvim)" ]; then
     # Install vim-plug
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-    
+
     if [ ! -d "$HOME/.config/nvim" ]; then
         mkdir -p "$HOME/.config/nvim"
     fi
@@ -36,3 +36,9 @@ else
     echo "nvim not installed"
     exit 1
 fi
+
+echo 'Install LSPs:'
+echo ':CocInstall coc-json coc-sh coc-clangd coc-go coc-git coc-html coc-tsserver coc-eslint coc-styled-components coc-markdownlint coc-pyright coc-rust-analyzer coc-zig'
+echo 'Install Treesitter language settings:'
+echo ':TSInstall bash c cpp css go hcl html javascript json lua make python regex rust sql toml tsx typescript yaml zig'
+
