7e821e9324efad3a5e925054332024e8865e724f
James Chung <james@chung.contact>
2023-05-09 01:39:54 -0500

    update vim.lua

--- lua/vim.lua
+++ lua/vim.lua
@@ -1,5 +1,5 @@
 vim.opt.list = true
-vim.opt.listchars:append "tab: →"
+vim.opt.listchars:append "tab: ⇥"
 vim.opt.listchars:append "trail:⋅"
 vim.opt.listchars:append "nbsp:⎵"
 -- vim.opt.listchars:append "trail:␠"
