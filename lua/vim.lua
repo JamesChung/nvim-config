a2934015d3955235b08ba58de9c896ccb03fab0f
James Chung <james@chung.contact>
2023-05-11 05:35:45 -0500

    update vim.lua

--- lua/vim.lua
+++ lua/vim.lua
@@ -1,8 +1,8 @@
 vim.opt.list = true
-vim.opt.listchars:append "multispace:⋅"
 vim.opt.listchars:append "trail:⋅"
 vim.opt.listchars:append "nbsp:⎵"
--- vim.opt.listchars:append "tab: ›"
+vim.opt.listchars:append "tab:› "
+-- vim.opt.listchars:append "multispace:⋅"
 -- vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "trail:␠"
 -- vim.opt.listchars:append "eol:↴"
