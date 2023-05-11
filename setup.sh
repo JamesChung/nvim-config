1a94b8d867de757a00c5f05c1bf04549abaef00f
James Chung <james@chung.contact>
2023-05-11 04:27:35 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 if [ ! -x "$(command -v brew)" ]; then
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    unameOut="$(uname -s)"
-    case "${unameOut}" in
-        Linux*)
-            (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.profile
-            eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-            ;;
-        Darwin*)
-            # TODO: Fill this in later
-            ;;
-    esac
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  unameOut="$(uname -s)"
+  case "${unameOut}" in
+    Linux*)
+      (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.profile
+      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+      ;;
+    Darwin*)
+      # TODO: Fill this in later
+      ;;
+  esac
 fi
 
 brew install neovim node gopls rust-analyzer terraform-ls zls
 
 if [ -x "$(command -v nvim)" ]; then
-    # Install vim-plug
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  # Install vim-plug
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-    if [ ! -d "$HOME/.config/nvim" ]; then
-        mkdir -p "$HOME/.config/nvim"
-    fi
+  if [ ! -d "$HOME/.config/nvim" ]; then
+    mkdir -p "$HOME/.config/nvim"
+  fi
 
-    if [ -x "$(command -v git)" ]; then
-        git clone https://github.com/JamesChung/nvim-config.git "$HOME/.config/nvim"
-    else
-        echo "You don't have git installed."
-        exit 1
-    fi
-    nvim -c 'PlugInstall | PlugUpdate'
-else
-    echo "nvim not installed"
+  if [ -x "$(command -v git)" ]; then
+    git clone https://github.com/JamesChung/nvim-config.git "$HOME/.config/nvim"
+  else
+    echo "You don't have git installed."
     exit 1
+  fi
+  nvim -c 'PlugInstall | PlugUpdate'
+else
+  echo "nvim not installed"
+  exit 1
 fi
 
 echo 'Install LSPs:'
