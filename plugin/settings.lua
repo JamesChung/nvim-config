666cd79e57ecae547f9daa52ed6ad42a36ac8ab3
James Chung <james@chung.contact>
2023-05-11 05:35:37 -0500

    update settings.lua

--- plugin/settings.lua
+++ plugin/settings.lua
@@ -5,11 +5,6 @@ require("toggleterm").setup {
 require("trouble").setup {
   position = "right",
 }
-require("indent_blankline").setup {
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
-}
 require("bufferline").setup {
   options = {
     buffer_close_icon = '',
