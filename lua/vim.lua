823b80ee7b3bc9c636afe708595b64345cd7de03
James Chung <james@chung.contact>
2023-05-13 09:34:34 -0500

    update vim.lua

--- lua/vim.lua
+++ lua/vim.lua
@@ -7,10 +7,11 @@ vim.opt.listchars:append "tab:› "
 -- vim.opt.listchars:append "trail:␠"
 -- vim.opt.listchars:append "eol:↴"
 
-require('rose-pine').setup({
-    --- @usage 'auto'|'main'|'moon'|'dawn'
-    variant = 'moon',
+require("rose-pine").setup({
+    --- @usage "auto"|"main"|"moon"|"dawn"
+    variant = "moon",
 })
-
 -- Set colorscheme after options
-vim.cmd('colorscheme rose-pine')
+vim.cmd("colorscheme rose-pine")
+-- Override theme highlight color
+vim.api.nvim_set_hl(0, "CursorColumn", { bg = "#44415a" })
