5719b26b2d09651be79cf0f839e8c4bbaad8b27a
James Chung <james@chung.contact>
2023-05-13 08:38:58 -0500

    update vim.lua

--- lua/vim.lua
+++ lua/vim.lua
@@ -6,3 +6,11 @@ vim.opt.listchars:append "tab:› "
 -- vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "trail:␠"
 -- vim.opt.listchars:append "eol:↴"
+
+require('rose-pine').setup({
+    --- @usage 'auto'|'main'|'moon'|'dawn'
+    variant = 'moon',
+})
+
+-- Set colorscheme after options
+vim.cmd('colorscheme rose-pine')
