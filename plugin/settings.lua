293d6678dc9f16d44a888e72d384f4fee299fcf0
James Chung <james@chung.contact>
2023-05-12 20:38:48 -0500

    update settings.lua

--- plugin/settings.lua
+++ plugin/settings.lua
@@ -1,28 +1,52 @@
+require("fidget").setup {}
+require("telescope").setup {}
 require("nvim-tree").setup {}
+require("nvim-web-devicons").setup {}
 require("toggleterm").setup {
-  direction = 'float',
+    direction = 'float',
 }
 require("trouble").setup {
-  position = "right",
+    position = "right",
+}
+require("mason").setup {
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        }
+    }
+}
+require("nvim-treesitter.configs").setup {
+    ensure_installed = {
+        "bash", "c", "cpp", "css", "go", "hcl",
+        "html", "javascript", "json", "lua", "make",
+        "python", "regex", "rust", "sql", "toml",
+        "tsx", "typescript", "yaml", "zig"
+    },
+    auto_install = true,
+    highlight = {
+        enabled = true,
+    }
 }
 require("bufferline").setup {
-  options = {
-    buffer_close_icon = '',
-    modified_icon = '●',
-    close_icon = '',
-    left_trunc_marker = '',
-    right_trunc_marker = '',
-    themable = true,
-    diagnostics = "coc",
-    sort_by = 'insert_at_end',
-    hover = {
-      enabled = true,
-      delay = 200,
-      reveal = { 'close' }
-    },
-    get_element_icon = function(element)
-      local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
-      return icon, hl
-    end
-  }
+    options = {
+        buffer_close_icon = '',
+        modified_icon = '●',
+        close_icon = '',
+        left_trunc_marker = '',
+        right_trunc_marker = '',
+        themable = true,
+        diagnostics = "coc",
+        sort_by = 'insert_at_end',
+        hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { 'close' }
+        },
+        get_element_icon = function(element)
+            local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
+            return icon, hl
+        end
+    }
 }
