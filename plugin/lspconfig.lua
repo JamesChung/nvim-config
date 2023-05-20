7d0071a5721ae08bc9d71bf18ca4d06ea358a1c7
James Chung <james@chung.contact>
2023-05-20 16:33:50 -0500

    update lspconfig.lua

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -1,11 +1,25 @@
 -- Setup language servers.
 require("mason-lspconfig").setup()
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero').preset({
+    float_border = "rounded",
+})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-  lsp.buffer_autoformat()
+    lsp.default_keymaps({ buffer = bufnr })
+    lsp.buffer_autoformat()
+    local opts = { buffer = bufnr }
+    local bind = vim.keymap.set
+    bind('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+    bind('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 end)
+
+lsp.set_sign_icons({
+    -- icons / text used for a diagnostic
+    error = "",
+    warn = "",
+    hint = "",
+    info = "",
+})
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
@@ -64,11 +78,11 @@ lsp.setup()
 local cmp = require('cmp')
 
 cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
-    ['<Tab>'] = cmp.mapping.confirm({select = false}),
+    mapping = {
+        -- `Enter` key to confirm completion
+        ['<Tab>'] = cmp.mapping.confirm({ select = false }),
 
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }
+        -- Ctrl+Space to trigger completion menu
+        ['<C-Space>'] = cmp.mapping.complete(),
+    }
 })
