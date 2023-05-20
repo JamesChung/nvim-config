bf722e94603a8bc90f378a83118cd13d03a3101b
James Chung <james@chung.contact>
2023-05-20 13:37:40 -0500

    update settings.lua

--- plugin/settings.lua
+++ plugin/settings.lua
@@ -9,7 +9,7 @@ require("lualine").setup {
     }
 }
 require("toggleterm").setup {
-    direction = 'float',
+    direction = "float",
 }
 require("trouble").setup {
     position = "right",
@@ -41,43 +41,43 @@ require("nvim-treesitter.configs").setup {
 }
 require("bufferline").setup {
     options = {
-        buffer_close_icon = '',
-        modified_icon = '●',
-        close_icon = '',
-        left_trunc_marker = '',
-        right_trunc_marker = '',
+        buffer_close_icon = "",
+        modified_icon = "●",
+        close_icon = "",
+        left_trunc_marker = "",
+        right_trunc_marker = "",
         themable = true,
         diagnostics = "coc",
-        sort_by = 'insert_at_end',
+        sort_by = "insert_at_end",
         hover = {
             enabled = true,
             delay = 200,
-            reveal = { 'close' }
+            reveal = { "close" }
         },
         get_element_icon = function(element)
-            local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
+            local icon, hl = require("nvim-web-devicons").get_icon_by_filetype(element.filetype, { default = false })
             return icon, hl
         end
     }
 }
 local dap = require("dap")
 dap.adapters.bashdb = {
-    type = 'executable',
-    command = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
-    name = 'bashdb',
+    type = "executable",
+    command = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/bash-debug-adapter",
+    name = "bashdb",
 }
 dap.configurations.sh = {
     {
-        type = 'bashdb',
-        request = 'launch',
+        type = "bashdb",
+        request = "launch",
         name = "Launch file",
         showDebugOutput = true,
-        pathBashdb = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb',
-        pathBashdbLib = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir',
+        pathBashdb = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb",
+        pathBashdbLib = vim.fn.stdpath("data") .. "/mason/packages/bash-debug-adapter/extension/bashdb_dir",
         trace = true,
         file = "${file}",
         program = "${file}",
-        cwd = '${workspaceFolder}',
+        cwd = "${workspaceFolder}",
         pathCat = "cat",
         pathBash = "/opt/homebrew/bin/bash",
         pathMkfifo = "mkfifo",
