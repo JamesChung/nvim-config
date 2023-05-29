c00ab272710467641ea1d2ded3f929b8cd0ca407
James Chung <james@chung.contact>
2023-05-29 14:32:20 -0500

    update lspconfig.lua

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -19,6 +19,7 @@ lsp.set_sign_icons({
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lspconfig = require("lspconfig")
+
 lspconfig.bashls.setup {
     capabilities = capabilities,
 }
