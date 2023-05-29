1ea8a074ac0d476e3ddfd1b966be9c4c786c909d
James Chung <james@chung.contact>
2023-05-29 17:30:22 -0500

    update lspconfig.lua

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -84,12 +84,39 @@ lspconfig.jsonls.setup {
 }
 lspconfig.lua_ls.setup {
     capabilities = capabilities,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 }
 lspconfig.pyright.setup {
     capabilities = capabilities,
 }
 lspconfig.rust_analyzer.setup {
     capabilities = capabilities,
+    settings = {
+        ["rust-analyzer"] = {
+            diagnostics = {
+                enable = false,
+            }
+        }
+    }
 }
 lspconfig.terraformls.setup {
     capabilities = capabilities,
