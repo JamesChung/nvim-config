5f35755453e9a4a60988140dcbedd0364632bbc6
James Chung <james@chung.contact>
2023-06-06 17:42:56 -0500

    update lspconfig.lua

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -20,6 +20,7 @@ local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lspconfig = require("lspconfig")
 local lsputil = require("lspconfig/util")
+local configs = require("lspconfig/configs")
 
 lspconfig.bashls.setup {
     capabilities = capabilities,
@@ -29,9 +30,7 @@ lspconfig.bufls.setup {
 }
 lspconfig.clangd.setup {
     capabilities = capabilities,
-    filetypes = {
-        "c", "cpp", "objc", "objcpp", "cuda",
-    },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
 }
 lspconfig.denols.setup {
     capabilities = capabilities,
@@ -50,6 +49,21 @@ lspconfig.dotls.setup {
 }
 lspconfig.eslint.setup {
     capabilities = capabilities,
+}
+if not configs.golangcilsp then
+    configs.golangcilsp = {
+        default_config = {
+            cmd = { 'golangci-lint-langserver' },
+            root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+            init_options = {
+                command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json",
+                    "--issues-exit-code=1" },
+            }
+        },
+    }
+end
+lspconfig.golangci_lint_ls.setup {
+    filetypes = { 'go', 'gomod' }
 }
 lspconfig.gopls.setup {
     capabilities = capabilities,
