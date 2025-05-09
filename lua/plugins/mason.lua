954ec312b25cfc4aac146f6e58cfea77f688f075
James Chung <james@chung.contact>
2025-05-09 12:20:20 -0500

    PATCH: Temporarily have to pin mason major version due to upstream breaking changes

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,23 +1,32 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        keys = {
-            {
-                "<leader>ma",
-                "<Cmd>Mason<CR>",
-                "Toggle Mason menu (Mason)",
-            },
-        },
-        config = function()
-            require("mason").setup({
-                ui = {
-                    icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        package_uninstalled = "✗",
-                    },
-                },
-            })
-        end,
+  {
+    "williamboman/mason.nvim",
+    -- Have to pin to major version 1 temporarily
+    -- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+    version = "^1.0.0",
+    keys = {
+      {
+        "<leader>ma",
+        "<Cmd>Mason<CR>",
+        "Toggle Mason menu (Mason)",
+      },
     },
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    -- Have to pin to major version 1 temporarily
+    -- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+    version = "^1.0.0",
+  },
 }
