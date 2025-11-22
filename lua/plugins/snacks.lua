f2faa816ef9845c1b851b293c88e385656c84a72
James Chung <james@chill.af>
2025-11-21 23:26:35 -0800

    feat: make searching files easier with sane defaults

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,5 +4,13 @@ return {
     explorer = {
       enabled = false,
     },
+    picker = {
+      sources = {
+        files = {
+          hidden = false,  -- Don't show dot-files by default
+          ignored = false, -- Don't show .gitignore files by default
+        },
+      },
+    },
   },
 }
