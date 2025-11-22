62c964348c832737fe3708318332452bbbf66ed1
James Chung <james@chill.af>
2025-11-22 01:34:42 -0800

    feat(plugins): disable background highlighting for markdown

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,9 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+        code = {
+            -- Disable background highlighting for all languages
+            disable_background = true,
+        },
+    },
+}
