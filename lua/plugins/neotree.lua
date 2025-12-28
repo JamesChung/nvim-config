b9480a3dfcd77ca82dba7b8149ab3dd502ad9347
James Chung <james_chung@apple.com>
2025-12-28 12:37:08 -0800

    refactor(neotree): remove custom component config overrides
    Simplifies neo-tree configuration by removing custom icon and git
    status symbol definitions. The plugin's default configuration will
    now be used for component styling and symbols, reducing maintenance
    overhead while keeping essential filesystem and window settings.

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,38 +2,6 @@ return {
 	"nvim-neo-tree/neo-tree.nvim",
 	opts = {
 		enable_git_status = true,
-		default_component_configs = {
-			icon = {
-				folder_closed = "",
-				folder_open = "",
-				folder_empty = "",
-				folder_empty_open = "",
-				default = "*",
-				highlight = "NeoTreeFileIcon",
-			},
-			modified = {
-				symbol = "[+]",
-				highlight = "NeoTreeModified",
-			},
-			name = {
-				trailing_slash = false,
-				use_git_status_colors = true,
-				highlight = "NeoTreeFileName",
-			},
-			git_status = {
-				symbols = {
-					added = "",
-					modified = "",
-					deleted = "",
-					renamed = "",
-					untracked = "",
-					ignored = "",
-					unstaged = "",
-					staged = "",
-					conflict = "",
-				},
-			},
-		},
 		filesystem = {
 			follow_current_file = {
 				enabled = true,
