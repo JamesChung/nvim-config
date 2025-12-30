dc9519ca09cb0240014fe1850405db55657c86a3
James Chung <james@chill.af>
2025-12-29 16:01:31 -0800

    cleanup config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,20 +10,14 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.lazyvim_picker = "snacks"
 
 -- Options
-vim.opt.number = true -- Show line numbers
-vim.opt.relativenumber = true -- Relative line numbers
 vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.tabstop = 4 -- Set tab width to 4 spaces
 vim.opt.softtabstop = 4 -- Set soft tabstop to 4 spaces
 vim.opt.shiftwidth = 4 -- Set indentation width to 4 spaces
 vim.opt.smartindent = true -- Smart autoindenting
 vim.opt.shiftround = true -- Round indent to multiple of shiftwidth
-vim.opt.incsearch = true
 vim.opt.wildmode = { "longest", "list" }
 vim.opt.scrolloff = 10
-
--- Colors
-vim.opt.termguicolors = true
 
 -- List Chars
 vim.opt.list = true
