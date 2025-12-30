34462a773f954d34c9fc5e6f2e9833e49be429a9
James Chung <james@chill.af>
2025-12-29 16:35:15 -0800

    cleanup more config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,28 +1,5 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	keys = {
-		{
-			"<leader>tf",
-			"<Cmd>Telescope find_files<CR>",
-			desc = "Find Files (Telescope)",
-		},
-		{
-			"<leader>tg",
-			"<Cmd>Telescope live_grep<CR>",
-			desc = "Live Grep (Telescope)",
-		},
-		{
-			"<leader>tb",
-			"<Cmd>Telescope buffers<CR>",
-			desc = "Show Buffers (Telescope)",
-		},
-		{
-			"<leader>th",
-			"<Cmd>Telescope help_tags<CR>",
-			desc = "Show help tags (Telescope)",
-		},
-	},
 	opts = {
 		defaults = {
 			-- Respect gitignore and exclude certain files
